5301c4fc866e8459ee8b28af02d79c76f2a17037
hhuczxy <983909419@qq.com>
2018-10-13 22:56:08 +0800

    TEST

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
+   static Tst t;
+   static List<Tst> list=new ArrayList<Tst>();
     public static void main(String[] args) {
-        //test
-        System.out.println("hello");
+
+
+        t= new Tst();
+        t.a=0;
+       list.add(t);
+        System.out.println("----"+list.get(0).a);
+        t.a=1;
+        t=null;
+        System.out.println("----"+list.get(0).a);
+    }
+
+   static class  Tst{
+        int a;
     }
 }
